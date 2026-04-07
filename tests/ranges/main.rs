80f0f573ac534ef1def97666eb28cc187d9e05da
Oleg Ianchenko <olegian@cs.washington.edu>
2026-04-06 17:26:58 -0700

    couple more tests

---
+++ tests/ranges/main.rs
@@ -0,0 +1,13 @@
+#![allow(unused)]
+
+#[ignore]
+fn main() {
+    let arr = [1; 100];
+    // let slice = 
+}
+
+fn foo(x: u32, y: u32, z: u32) -> u32 {
+    let tmp = x + y;
+
+    tmp
+}
---
+++ tests/ranges/mod.rs
@@ -0,0 +1,30 @@
+use std::path::Path;
+
+use crate::common::{ExpectedOutput, ExpectedSite, compile_and_execute, delete, verify};
+
+// TODO: FINISH THIS TEST
+#[test]
+fn simple() {
+    let mut expected = ExpectedOutput::new();
+    expected.register_site(ExpectedSite::new("main::ENTER"));
+    expected.register_site(ExpectedSite::new("main::EXIT"));
+    expected.register_site(
+        ExpectedSite::new("foo::ENTER")
+            .register("x", 0)
+            .register("y", 1)
+            .register("z", 2),
+    );
+    expected.register_site(
+        ExpectedSite::new("foo::EXIT")
+            .register("x", 0)
+            .register("y", 0)
+            .register("z", 1)
+            .register("RET", 0),
+    );
+
+    let executable = Path::new(file!()).parent().unwrap().join("simple.out");
+    delete(&executable);
+
+    let ati_output = compile_and_execute(&executable);
+    verify(&ati_output, expected.inner());
+}
